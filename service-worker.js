45a5f7e332f95ba0399043cb582589a6b31105d7
Yoichi Adachi <yoichi.adachi@brandwidth.co.uk>
2019-09-11 12:17:19 +0100

    update worker

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,17 @@
-this.addEventListener('install', function(e) {
-  //console.log('service worker install', e);
+self.addEventListener('install', function(e) {
+  console.log('service worker install');
+  console.log(e);
   e.waitUntil(
     caches.open('v1').then(function(cache) {
-      //console.log('cache');
+      console.log('cache');
       return cache.addAll([
         '/index.html',
         '/js/main.89c19c4e9b69a833aed9.js',
         '/css/main.6cd85c63120b6d0833c3.css',
-        '/puzzles/precure01.jpg'
+        '/images/b8c72eb3b354ec22de12e2fb82acfea8.woff',
+        '/images/9c9c9594083c7803650bca0f43f2b326.gif',
+        '/puzzles/precure01.jpg',
+        '/puzzles/precure02.jpg'
       ]);
     })
   );
@@ -18,5 +22,6 @@ self.addEventListener('activate', function(e) {
 });
 
 self.addEventListener('fetch', function(e) {
-  console.log('fetch', e);
+  console.log('fetch');
+  console.log(e.request.url);
 });
