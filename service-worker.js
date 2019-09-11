b39fe571736f344ca0eda230dbc1f805bc5e8222
Yoichi Adachi <yoichi.adachi@brandwidth.co.uk>
2019-09-11 11:10:14 +0100

    update worker

--- service-worker.js
+++ service-worker.js
@@ -2,11 +2,19 @@
 
 self.addEventListener('install', function(e) {
   console.log('service worker install');
-  
+  e.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        './index.html'    
+      ]);
+    });
+  );
 });
 
 self.addEventListener('activate', function(e) {
   console.log('service worker activate');
 });
 
-self.addEventListener('fetch', function(e) {});
+self.addEventListener('fetch', function(e) {
+  console.log('fetch', e);
+});
